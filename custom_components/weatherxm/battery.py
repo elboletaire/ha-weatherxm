16163098a92017c09ea22c360ff39f4a34e5f6f7
Sam <sam.ley@gmail.com>
2024-10-20 22:21:28 -0600

    Add stateclass to battery

--- custom_components/weatherxm/battery.py
+++ custom_components/weatherxm/battery.py
@@ -1,5 +1,11 @@
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
-from homeassistant.components.sensor import SensorEntity
+
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorEntity,
+    SensorEntityDescription,
+    SensorStateClass,
+)
 
 BATTERY_LEVEL_MAP = {
     "ok": 100.0,
@@ -16,6 +22,8 @@ class WeatherXMBatteryLevelSensor(CoordinatorEntity, SensorEntity):
         self._is_active = is_active
         self._attr_name = f"{alias} Battery"
         self._attr_unique_id = f"{alias}_battery"
+        self._attr_state_class = SensorDeviceClass.BATTERY
+        self._attr_state_class = SensorStateClass.MEASUREMENT
 
     @property
     def state(self):
